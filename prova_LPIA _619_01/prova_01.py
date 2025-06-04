7bb8f088deb5c99f6d08759ebb812acb35e6806e
Luccas Fernandes <fernandesm.luccas@gmail.com>
2025-06-04 16:00:35 -0300

    prova_01_LPIA

---
+++ prova_LPIA _619_01/prova_01.py
@@ -0,0 +1,9 @@
+# Solicitando base e altura do retângulo
+base = float(input("Digite a base do retângulo: "))
+altura = float(input("Digite a altura do retângulo: "))
+
+# Calculando a área do retângulo
+area = base * altura
+
+# Exibindo o resultado
+print(f"A área do retângulo é: {area:.2f}")
